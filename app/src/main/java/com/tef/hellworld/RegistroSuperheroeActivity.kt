fe5ff49b76bf1d701d20a5d87d619f7cb99c6b41
EstefaniaRamirezV <estefi299b@gmail.com>
2021-11-19 15:19:10 -0500

    Created user interface

---
+++ app/src/main/java/com/tef/hellworld/RegistroSuperheroeActivity.kt
@@ -0,0 +1,58 @@
+package com.tef.hellworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import com.google.android.material.textfield.TextInputEditText
+
+class RegistroSuperheroeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_superheroe)
+
+        // var o val para crear objetos, val es estático, var es mutable
+        // findViewById(R.id.registrar_button) con esto conecto el objeto con el recurso del layout
+
+        val registrarButton : Button = findViewById(R.id.registrar_button)
+        val nameEditText :  EditText = findViewById(R.id.name_edit_text)
+        val infoTextView : TextView = findViewById(R.id.info_text_view)
+        val heightEditText : TextInputEditText = findViewById(R.id.height_edit_text)
+        val maleRadioButton : RadioButton = findViewById(R.id.male_radio_button)
+        val femaleRadioButton : RadioButton = findViewById(R.id.female_radio_button)
+        val technologyCheckBox : CheckBox = findViewById(R.id.technology_check_box)
+        val speedCheckBox : CheckBox = findViewById(R.id.speed_check_box)
+        val moneyCheckBox : CheckBox = findViewById(R.id.money_check_box)
+        val citySpinner : Spinner = findViewById(R.id.city_spinner)
+
+
+        // método setOnClickListener para añadir funcionalidad al botón
+
+        registrarButton.setOnClickListener {
+
+            // .text es equivalente a getText()
+            // con esto asigno el nombre a infoTextView
+
+            val name : String = nameEditText.text.toString()
+            val height : Float = heightEditText.text.toString().toFloat()
+            var powers = ""
+            val city = citySpinner.selectedItem.toString()
+
+            val gender = if (maleRadioButton.isChecked)
+                getString(R.string.male)
+            else
+                getString(R.string.female)
+
+            if (technologyCheckBox.isChecked)
+                powers = getString(R.string.technology)
+            if (speedCheckBox.isChecked)
+                powers = powers + " " +  getString(R.string.speed)
+            if (moneyCheckBox.isChecked)
+                powers = powers + " " + getString(R.string.money)
+
+
+            // infoTextView.text = "Nombre: " + name + ", Estatura: " + height
+            infoTextView.text = getString(R.string.name_info, name, height, gender, powers, city)
+
+        }
+    }
+}
